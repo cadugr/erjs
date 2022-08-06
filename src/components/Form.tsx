d86c079fb2a11e11ec1bfd91b5657e1a091daaa2
Cadu Guerra <cadu.guerra@gmail.com>
2022-08-06 19:20:20 -0300

    Finalizando a aula 2.30

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,57 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 export default function Form () {
-    const [name, setName] = useState('Daniel')
+    const [form, setForm] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        password: ''
+    })
 
-    return <div>
+    function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log(form)
+    }
+
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setForm({
+            ...form, 
+            //extraindo o valor de forma dinamica
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return <form onSubmit={handleSubmit}>
         <input 
         placeholder="Nome"
+        name="name"
         type="text"
-        value={name}
-        onChange={e => setName(e.target.value)}
+        value={form.name}
+        onChange={handleInputChange}
         />
-        <div>
-            {name}
-        </div>
-    </div>
+        <input 
+        placeholder="Sobrenome"
+        name="lastName"
+        type="text"
+        value={form.lastName}
+        onChange={handleInputChange}
+        />
+        <input 
+        placeholder="Email"
+        name="email"
+        type="email"
+        value={form.email}
+        onChange={handleInputChange}
+        />
+        <input 
+        placeholder="Senha"
+        name="password"
+        type="password"
+        value={form.password}
+        onChange={handleInputChange}
+        />
+        <button type="submit">
+            enviar formulário
+        </button>
+    </form>
 }
