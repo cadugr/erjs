e32f257afb68fed8ffe4d942296c47e025e54cfe
Cadu Guerra <cadu.guerra@gmail.com>
2022-08-06 23:07:59 -0300

    Finalizando a aula 3.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 
 function App() {
-  const names = ['Daniel', 'Thiago', 'Alex']
+  const [names, setNames] = useState(['Daniel', 'Thiago', 'Alex', 'Jão', 'Alex'])
+
+  //const[toggle, setToggle] = useState(false)
+
+  useEffect(() => {
+    //esse bloco de código é executado no momento que o componente foi montado
+    console.log('componente foi montado')
+
+    return () => {
+      console.log('componente foi desmontado')
+    }
+  }, [])
+
+  /**Podemos adicionar vários useEffect.  Podemos também determinar um retorno como uma função para o nosso useEffect.
+   * Isso fará com que seja executado assim que o nosso componente for desmontado.
+   */
+
+  // useEffect(() => {
+  //   console.log('toggle', toggle)
+  // }, [toggle])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +32,11 @@ function App() {
             names.map((name, index) => <li key={index}>{name}</li>)
            } 
         </ul> 
+        <button onClick={() => 
+          setNames([...names, 'jeremias'])
+        }>
+          adicionar jeremias
+        </button>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+function Batata () {
+  const [canViewApp, setCanViewApp] = useState(true)
+
+  return <>
+    {
+      canViewApp &&
+      <App/>
+    }
+    <button onClick={() => 
+          setCanViewApp(!canViewApp)
+        }>
+          toogle
+        </button>
+  </>
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Batata />
   </React.StrictMode>
 );
